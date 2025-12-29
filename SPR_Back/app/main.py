e65419888280b55b23da3219ea4ee2d3249179f0
Chetan <chetan132005@gmail.com>
2025-12-29 23:51:39 +0530

    Final

--- SPR_Back/app/main.py
+++ SPR_Back/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://127.0.0.1:8080",
-        "https://spr-smart-place-recommender.vercel.app",
+        "https://sprsmartplacerecommender.vercel.app",
     ],  # frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
